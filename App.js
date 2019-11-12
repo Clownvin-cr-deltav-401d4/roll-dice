8a2743e785bad9bd5ffca5c48e6e1f548782994d
Calvin G Hall <123chall@gmail.com>
2019-11-11 22:25:20 -0600

    Got started on dice tool

---
+++ App.js
@@ -0,0 +1,51 @@
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
+import React, { useState } from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+
+import HomeScreen from './screens/HomeScreen';
+
+export default function App(props) {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return (
+      <AppLoading
+        startAsync={loadResourcesAsync}
+        onError={handleLoadingError}
+        onFinish={() => handleFinishLoading(setLoadingComplete)}
+      />
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <HomeScreen />
+      </View>
+    );
+  }
+}
+
+async function loadResourcesAsync() {
+  await Promise.all([
+    Asset.loadAsync([
+    ]),
+  ]);
+}
+
+function handleLoadingError(error) {
+  // In this case, you might want to report the error to your error reporting
+  // service, for example Sentry
+  console.warn(error);
+}
+
+function handleFinishLoading(setLoadingComplete) {
+  setLoadingComplete(true);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,202 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Slider,
+} from 'react-native';
+
+import { Accelerometer } from 'expo-sensors';
+import { useKeepAwake } from 'expo-keep-awake';
+
+const WAITING = 'Waiting for rolling motion...', ROLLING = 'Rolling...';
+
+export default function HomeScreen() {
+  useKeepAwake();
+
+  let [dice, setDice] = useState(Array(100).fill(1));
+  let [state, setState] = useState(WAITING);
+  let [sides, setSides] = useState(6);
+  let [numDice, setNumDice] = useState(1);
+  let [sensorData, setSensorData] = useState({
+    available: true,
+    x: 0,
+    y: 0,
+    z: 1,
+    velocity: 0,
+  });
+
+  const getRoll = () => {
+    return Math.floor(Math.random() * (sides - 1)) + 1;
+  }
+
+  let subscription;
+
+  const changeSides = sides => {
+    setSides(Math.round(sides));
+  }
+
+  const changeNumDice = num => {
+    setNumDice(Math.round(num));
+  }
+
+  const changeSensorData = data => {
+    setSensorData(sensorData => ({
+      ...sensorData, 
+      velocity: (Math.abs(sensorData.x - data.x) + Math.abs(sensorData.y - data.y) + Math.abs(sensorData.z - data.z)) / 3,
+      ...data}));
+  }
+
+  useEffect(() => {
+    Accelerometer.isAvailableAsync().then(available => {
+      if (available) {
+        Accelerometer.setUpdateInterval(100);
+        setSensorData({...sensorData, available: true});
+        subscription = Accelerometer.addListener(changeSensorData);
+      } else {
+        setSensorData({...sensorData, available: false});
+      }
+    });
+  }, []);
+
+  const getDice = () => {
+    let diceComponents = [];
+    for (let i = 0; i < numDice; i++) {
+      diceComponents.push(<Text key={i} style={styles.die}>{state === WAITING ? dice[i] : getRoll()}</Text>);
+    }
+    return diceComponents;
+  };
+
+  if (sensorData.velocity > 1.2 && state !== ROLLING) {
+    setState(ROLLING);
+  }
+
+  if (sensorData.velocity < 1.2 && state !== WAITING) {
+    setState(WAITING);
+    setDice(dice => dice.map((die, index) => index < numDice ? Math.round(Math.random() * (sides - 1)) + 1 : die));
+  }
+
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+        <Text style={styles.getStartedText}>Shake to roll {numDice} {sides} sided dice...</Text>
+        <Slider onValueChange={changeSides} value={sides} minimumValue={2} maximumValue={20}></Slider>
+        <Slider onValueChange={changeNumDice} value={numDice} minimumValue={1} maximumValue={100}></Slider>
+        <Text style={styles.getStartedtext}>Velocity: {sensorData.velocity}</Text>
+        <Text style={styles.getStartedtext}>State: {state}</Text>
+        <ScrollView
+          style={styles.diceContainer}
+          contentContainerStyle={styles.contentContainer}>
+          {getDice()}
+        </ScrollView>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  diceContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  die: {
+    borderWidth: 2,
+    width: 100,
+    height: 100,
+    margin: 10,
+    borderRadius: 5,
+    borderColor: '#222',
+    textAlign: 'center',
+    fontSize: 60,
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
